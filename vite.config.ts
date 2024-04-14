4e39edee8a32b2b5915878b5f605aea1c48d5017
Misha Merkushin <mikhail.merkushin@sbermarket.ru>
2024-04-14 16:04:34 +0300

    feat: build assets to git

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: `assets/[name].js`,
+                chunkFileNames: `assets/[name].js`,
+                assetFileNames: `assets/[name].[ext]`
+            }
+        }
+    },
     plugins: [react()],
     define: {
         'process.env': process.env,
