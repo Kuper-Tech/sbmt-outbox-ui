73eea5b1fe5b242fe763329c7e63c84702bcde56
Misha Merkushin <mikhail.merkushin@sbermarket.ru>
2024-05-01 16:43:24 +0300

    fix: inbox classes list

--- src/components/InboxClasses.tsx
+++ src/components/InboxClasses.tsx
@@ -1,9 +1,10 @@
-import { List, Datagrid, TextField } from "react-admin";
+import { List, Datagrid, TextField, BooleanField } from "react-admin";
 
 export const InboxClassesList = () => (
     <List>
         <Datagrid rowClick="edit">
-            <TextField source="name" />
+            <TextField source="id" />
+            <BooleanField source="polling_enabled" />
         </Datagrid>
     </List>
 );
